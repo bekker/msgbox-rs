f17062cb96d5f410e14aaa071645508c7fda73ff
Jang Ryeol <ryeolj5911@gmail.com>
2017-01-20 16:59:44 +0900

    TOPMOST to SYSTEMMODAL

--- src/windows.rs
+++ src/windows.rs
@@ -7,15 +7,15 @@ pub fn create(title:&str, content:&str, icon_type:IconType) {
     use std::ffi::CString;
     use std::ptr::null_mut;
     use self::user32::MessageBoxA;
-    use self::winapi::winuser::{MB_OK, MB_ICONINFORMATION, MB_ICONERROR, MB_TOPMOST};
+    use self::winapi::winuser::{MB_OK, MB_ICONINFORMATION, MB_ICONERROR, MB_SYSTEMMODAL};
 
     let lp_text = CString::new(content).unwrap();
     let lp_caption = CString::new(title).unwrap();
 
     let window_type = match icon_type {
-        IconType::ERROR => MB_OK | MB_ICONERROR | MB_TOPMOST,
-        IconType::INFO => MB_OK | MB_ICONINFORMATION | MB_TOPMOST,
-        IconType::NONE => MB_OK | MB_TOPMOST,
+        IconType::ERROR => MB_OK | MB_ICONERROR | MB_SYSTEMMODAL,
+        IconType::INFO => MB_OK | MB_ICONINFORMATION | MB_SYSTEMMODAL,
+        IconType::NONE => MB_OK | MB_SYSTEMMODAL,
     };
 
     unsafe {
