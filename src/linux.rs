4f7e7fa8722c622fb9c7d4e57068ea81d7d58093
Jang Ryeol <ryeolj5911@gmail.com>
2017-01-20 17:34:25 +0900

    linux icons

--- src/linux.rs
+++ src/linux.rs
@@ -1,40 +1,29 @@
 extern crate gtk;
 use self::gtk::prelude::*;
+use self::gtk::{ButtonsType, DialogFlags, MessageType, MessageDialog, Window};
 
-pub fn create(title:&str, content:&str, btn_text:&str) {
+use icon::IconType;
+
+pub fn create(title:&str, content:&str, icon_type:IconType) {
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
         println!("{}", content);
         return;
     }
 
-    let window = gtk::Window::new(gtk::WindowType::Toplevel);
+    let message_type = match icon_type {
+        IconType::ERROR => MessageType::Error,
+        IconType::INFO => MessageType::Info,
+        IconType::NONE => MessageType::Other,
+    };
 
-    window.set_title(title);
-    window.set_border_width(10);
-    window.set_position(gtk::WindowPosition::Center);
-    window.set_size_request(350, 70);
-    window.set_resizable(false);
-
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-
-    let hbox = gtk::Box::new(gtk::Orientation::Vertical, 20);
-
-    let label = gtk::Label::new(Some(content));
-    hbox.add(&label);
-
-    let button = gtk::Button::new_with_label(btn_text);
-
-    button.connect_clicked(move |_| {
-        gtk::main_quit();
-        Inhibit(false);
-    });
-    hbox.add(&button);
-
-    window.add(&hbox);
-    window.show_all();
-    gtk::main();
+    let dialog = MessageDialog::new(
+            None::<&gtk::Window>,
+            DialogFlags::empty(),
+            message_type,
+            ButtonsType::Ok,
+            content);
+    dialog.set_title(title);
+    dialog.run();
+    dialog.destroy();
 }
